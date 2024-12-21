7ab65120fe0460ae9b634cba98a16eec010e1009
Manojkumar962952 <babamani002@gmail.com>
2024-12-21 10:46:32 +0530

    first commit

---
+++ node_service/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 5002;
+
+// Simple route to return a message
+app.get('/', (req, res) => {
+    res.send('Hello from Service 2 (Node.js Express)!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Service 2 running on http://localhost:${port}`);
+});
+
